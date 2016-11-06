c49c5b79447021625b326017653ebbf110da0716
ohmyxm <wmx2112@gmail.com>
2016-11-06 16:59:27 +0800

    finished version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+# gradient-color README.md
 # gradient-color
 Make beautiful gradient color
+
+## USAGE
+
+```js
+var getGradient = require('gradient-color').getGradient
+
+var number = 30 // How many units between the two colors
+var startColor = "#7e93f5" // start color, must in hex mode
+var endColor = "#f32b65" // end color, must in hex mode
+
+var colors = getGradient(number, startColor, endColor)
+// output an array, each unit is a color string in "rgb(r,g,b)" format
+```
+
+
+## TODOS
+
+1. [ ] Support all color format (input and output)
+2. [ ] Add color stops
+3. [ ] Add test file
---
+++ index.js
@@ -0,0 +1,43 @@
+exports.getGradient = function(n, startColor, endColor) {
+	// add semicolon cause `[` is the at starting char of a line
+	let Rx, Gx, Bx, Ry, Gy, By;
+	[Rx, Gx, Bx] = hexToRgb(startColor);
+	[Ry, Gy, By] = hexToRgb(endColor);
+
+	let newColors = []
+	for (var i = 0; i <= n; i++) {
+		let r, g, b
+		let percent = i / n;
+		[r, g, b] = [
+			calcColor(Rx, Ry, percent), 
+			calcColor(Gx, Gy, percent), 
+			calcColor(Bx, By, percent)
+		]
+
+		newColors.push(`rgb(${r},${g},${b})`)
+	}
+
+	return newColors
+}
+// function linearGradient(n, ...args) {
+// 	args.forEach(color => {
+
+// 	})
+// }
+
+function calcColor(x, y, percent) {
+	return x + Math.floor((y - x) * percent)
+}
+function hexToRgb(hex) {
+  let rgb = []
+  hex = hex.substr(1)
+
+  // "#abc" -> "#aabbcc"
+  if (hex.length === 3) {
+    hex = hex.replace(/(.)/g, '$1$1')
+  }
+  hex.replace(/../g, color => rgb.push(parseInt(color, 0x10)))
+
+  return rgb
+}
+
